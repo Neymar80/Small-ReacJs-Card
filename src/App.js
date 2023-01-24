eebb8de7a038d2a4e6a73f679001166d33deaf6f
Phaneendra Thupakula <112691154+Neymar80@users.noreply.github.com>
2023-01-24 12:48:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Bootstrapcard from './Boostrapcard';
+
+function App() {
+  return (
+    <div className="App">
+      <Bootstrapcard
+        title='NeymarJr_10'
+        imageUrl='https://c4.wallpaperflare.com/wallpaper/301/300/10/neymar-4k-wallpaper-thumb.jpg'
+        body='Neymar da Silva Santos Júnior, known as Neymar, is a Brazilian professional footballer who plays as a forward for Ligue 1 club Paris Saint-Germain and the Brazil national team. A prolific goalscorer and renowned playmaker, he is regarded as one of the best players in the world.'
+        />
+    </div>
+  );
+}
+export default App;
